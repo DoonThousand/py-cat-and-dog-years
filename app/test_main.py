99fe3e2430be86d2c3347500280e94a5de21e9bb
Admin <valerasarkisan285@gmail.com>
2026-05-02 21:42:37 +0300

    py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -3,17 +3,17 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age,dog_age,expected",
+    "cat_age, dog_age, result",
     [
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
+        (23, 27, [1, 2]),
         (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (29, 29, [3, 3]),
         (100, 100, [21, 17]),
     ]
 )
-def test_cat_dog_age(cat_age: int, dog_age: int, expected: list) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+def test_calculate_age(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
